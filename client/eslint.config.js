0a44459ec96265abcfd1f0e09dceb1d25d8b463a
Ernest Moyo <ernestmoyo35@gmail.com>
2026-02-05 16:28:10 +0300

    fix: add client ESLint config and force-exit for backend tests

---
+++ client/eslint.config.js
@@ -0,0 +1,15 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  {
+    files: ['src/**/*.{js,jsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
--- server/jest.config.js
+++ server/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testMatch: ['**/tests/**/*.test.js'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.js', '!src/index.js'],
+  forceExit: true,
 };
